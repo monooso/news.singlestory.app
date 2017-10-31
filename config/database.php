78de78b7ac974465e64ee5d7b799cdcabef8784e
Stephen Lewis <stephen@experiencehq.net>
2017-10-30 23:09:07 -0700

    Use MySQL for development.

--- config/database.php
+++ config/database.php
@@ -5,9 +5,18 @@ return [
     'migrations'  => 'migrations',
     'connections' => [
         'development' => [
-            'driver'   => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('development.sqlite')),
-            'prefix'   => '',
+            'driver'      => 'mysql',
+            'host'        => env('DB_HOST'),
+            'port'        => env('DB_PORT'),
+            'database'    => env('DB_DATABASE'),
+            'username'    => env('DB_USERNAME'),
+            'password'    => env('DB_PASSWORD'),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset'     => 'utf8mb4',
+            'collation'   => 'utf8mb4_unicode_ci',
+            'prefix'      => '',
+            'strict'      => true,
+            'engine'      => null,
         ],
 
         'testing' => [
