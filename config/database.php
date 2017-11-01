286a6456bf1752d181948f9bbdfffc72ca905b3e
Stephen Lewis <stephen@experiencehq.net>
2017-10-31 17:08:09 -0700

    Add 'production' database config.

--- config/database.php
+++ config/database.php
@@ -19,19 +19,13 @@ return [
             'engine'      => null,
         ],
 
-        'testing' => [
-            'driver'   => 'sqlite',
-            'database' => ':memory:',
-            'prefix'   => '',
-        ],
-
-        'mysql'  => [
+        'production'  => [
             'driver'      => 'mysql',
-            'host'        => env('DB_HOST', '127.0.0.1'),
-            'port'        => env('DB_PORT', '3306'),
-            'database'    => env('DB_DATABASE', 'forge'),
-            'username'    => env('DB_USERNAME', 'forge'),
-            'password'    => env('DB_PASSWORD', ''),
+            'host'        => env('DB_HOST'),
+            'port'        => env('DB_PORT'),
+            'database'    => env('DB_DATABASE'),
+            'username'    => env('DB_USERNAME'),
+            'password'    => env('DB_PASSWORD'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset'     => 'utf8mb4',
             'collation'   => 'utf8mb4_unicode_ci',
@@ -40,17 +34,10 @@ return [
             'engine'      => null,
         ],
 
-        'pgsql'  => [
-            'driver'   => 'pgsql',
-            'host'     => env('DB_HOST', '127.0.0.1'),
-            'port'     => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset'  => 'utf8',
+        'testing' => [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
             'prefix'   => '',
-            'schema'   => 'public',
-            'sslmode'  => 'prefer',
         ],
     ],
 ];
