163ace8bd017988d82bf31e506d74b6bf3e1c412
Stephen Lewis <stephen@experiencehq.net>
2017-12-17 19:05:05 -0800

    Add News API “source” config variable

--- config/news.php
+++ config/news.php
@@ -4,7 +4,10 @@ return [
     // The number of "most popular" items to process.
     'limit' => 5,
 
-    'newsapi' => ['api_key' => env('NEWSAPI_API_KEY')],
+    'newsapi' => [
+        'api_key' => env('NEWSAPI_API_KEY'),
+        'source'  => env('NEWSAPI_SOURCE'),
+    ],
 
     'nytimes' => [
         'api_key' => env('NYTIMES_API_KEY'),
