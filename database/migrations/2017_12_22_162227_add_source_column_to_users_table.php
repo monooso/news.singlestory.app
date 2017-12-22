16d6719f87e68f8ce7a2b4e1446466d4f0ad88e9
Stephen Lewis <stephen@experiencehq.net>
2017-12-22 17:52:18 +0000

    Fix use statement order, to appease StyleCI

--- database/migrations/2017_12_22_162227_add_source_column_to_users_table.php
+++ database/migrations/2017_12_22_162227_add_source_column_to_users_table.php
@@ -1,9 +1,9 @@
 <?php
 
 use App\Constants\NewsSource;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class AddSourceColumnToUsersTable extends Migration
 {
