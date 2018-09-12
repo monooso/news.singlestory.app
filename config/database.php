e0674b5f5e71e33beee4a08dc624d6b24b23f3c7
Stephen Lewis <stephen@experiencehq.net>
2018-09-11 20:38:45 -0700

    Remove duplicate database config

--- config/database.php
+++ config/database.php
@@ -1,25 +1,10 @@
 <?php
 
 return [
-    'default'     => env('DB_CONNECTION', 'development'),
+    'default'     => env('DB_CONNECTION', 'mysql'),
     'migrations'  => 'migrations',
     'connections' => [
-        'development' => [
-            'driver'      => 'mysql',
-            'host'        => env('DB_HOST'),
-            'port'        => env('DB_PORT'),
-            'database'    => env('DB_DATABASE'),
-            'username'    => env('DB_USERNAME'),
-            'password'    => env('DB_PASSWORD'),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset'     => 'utf8mb4',
-            'collation'   => 'utf8mb4_unicode_ci',
-            'prefix'      => '',
-            'strict'      => true,
-            'engine'      => null,
-        ],
-
-        'production' => [
+        'mysql' => [
             'driver'      => 'mysql',
             'host'        => env('DB_HOST'),
             'port'        => env('DB_PORT'),
